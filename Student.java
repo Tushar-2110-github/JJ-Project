2f9aae459ef2d0bfc9445eaa1078b21a90740a90
Tushar-2110-github <143584287+Tushar-2110-github@users.noreply.github.com>
2024-04-09 19:06:22 +0530

    commiting hello everyone

--- Student.java
+++ Student.java
@@ -2,4 +2,8 @@ public class Student {
     public void sayHello(){
         System.out.println("Say Hello to me");
     }
+
+    public void m(){
+        System.out.println("Hello Everyone");
+    }
 }
